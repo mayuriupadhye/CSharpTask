b666a27e492b0c936f28d24529318e112b309276
mayuriupadhye <upadhyemayu27@gmail.com>
2021-12-31 15:23:55 +0530

    CSharp coding test

---
+++ Task1DEC23/Task1DEC23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1DEC23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student(1, "Mayuri", "c#");
+            s.Payment(1000);
+            s.Print();
+            Console.WriteLine("DueAmount : " + s.DueAmount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task1DEC23/Task1DEC23/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1DEC23
+{
+    class Student
+    {
+        private int rollno;
+        private string name;
+        private string course;
+        private int feepaid;
+
+        public Student(int rollno, string name, string course)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.course = course;
+        }
+        public void Payment(int amount)
+        {
+            feepaid += amount;
+        }
+        public void Print()
+        {
+            Console.WriteLine(rollno);
+            Console.WriteLine(name);
+            Console.WriteLine(course);
+            Console.WriteLine(feepaid);
+        }
+        public int DueAmount
+        {
+            get
+            {
+                return Totalfee - feepaid;
+            }
+        }
+        public int Totalfee
+        {
+            get
+            {
+                return course == "c#" ? 2000 : 3000;
+            }
+        }
+    }
+}
