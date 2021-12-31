02af3fa913ec17e594c54133ca94a78ae2a57f58
mayuriupadhye <upadhyemayu27@gmail.com>
2021-12-31 16:06:59 +0530

    CSharp coding test-2

---
+++ Task2DEC23/Task2DEC23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2DEC23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo s = new StudentInfo(111, "Pooja", "asp.net");
+            s.Payment(1000);
+            s.Print();
+            Console.WriteLine("Dueamount : " + s.DueAmount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2DEC23/Task2DEC23/StudentInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2DEC23
+{
+    class StudentInfo
+    {
+        private int rollno;
+        private string name;
+        private string course;
+        private int feepaid;
+        private static double servicetax = 12.3;
+        public StudentInfo(int rollno, string name, string course)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.course = course;
+        }
+        public void Payment(int amount)
+        {
+            feepaid += amount;
+        }
+        public void Print()
+        {
+            Console.WriteLine(rollno);
+            Console.WriteLine(name);
+            Console.WriteLine(course);
+            Console.WriteLine(feepaid);
+        }
+        public int DueAmount
+        {
+            get
+            {
+                return Totalfee - feepaid;
+            }
+        }
+        public int Totalfee
+        {
+            get
+            {
+                double total = course == "c#" ? 2000 : 3000;
+                //service tax
+                total = total + total * servicetax / 100;
+                return (int)total;
+            }
+        }
+        public static double ServiceTax
+        {
+            get
+            {
+                return servicetax;
+            }
+            set
+            {
+                servicetax = value;
+            }
+        }
+    }
+}
