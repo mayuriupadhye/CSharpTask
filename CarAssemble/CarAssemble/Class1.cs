5fe563149a9557eee616a60e129c8d5d1522eb31
mayuriupadhye <upadhyemayu27@gmail.com>
2022-01-05 15:38:34 +0530

    Cars Assemble

---
+++ CarAssemble/CarAssemble/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarAssemble
+{
+    class Class1
+    {
+        public static int hr = 221;
+        public double SuccessRate(int speed)
+        {
+            if (speed >= 1 && speed <= 4)
+            {
+                return 1.0;
+            }
+            if (speed >= 5 && speed <= 8)
+            {
+                return 0.9;
+            }
+            if (speed <= 9)
+            {
+                return 0.8;
+            }
+            if (speed == 10)
+            {
+                return 0.77;
+            }
+            return speed;
+
+        }
+        public double ProductionRatePerHr(int speed)
+        {
+            return hr * speed * SuccessRate(speed);
+        }
+        public int WorkingItemsPerMin(int speed)
+        {
+            return (int)ProductionRatePerHr(speed) / 60;
+        }
+
+    }
+}
---
+++ CarAssemble/CarAssemble/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarAssemble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            Console.WriteLine(c.SuccessRate(10));
+            Console.WriteLine(c.ProductionRatePerHr(6));
+            Console.WriteLine(c.WorkingItemsPerMin(6));
+            Console.ReadLine();
+        }
+    }
+}
