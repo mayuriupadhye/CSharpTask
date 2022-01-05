810727b88f2157c18ba86c24c349e7454c1e9f50
mayuriupadhye <upadhyemayu27@gmail.com>
2022-01-05 12:25:16 +0530

    File Handling Demo

---
+++ File_Handling/File_Handling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"F:\C#_Programs\example_1.txt";
+            //Creating and Writting
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                writer.Write(DateTime.Now.ToString());
+                Console.WriteLine("Successfully Added Current Date and Time");
+            }
+            //Reading File
+            using (StreamReader reader = new StreamReader(file))
+            {
+                Console.Write("Reading Current Time : ");
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            Console.ReadKey();
+        }
+    }
+}
