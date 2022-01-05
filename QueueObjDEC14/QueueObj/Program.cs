1bad03daf100c514e13987000b2359a9db4bc0e2
mayuriupadhye <upadhyemayu27@gmail.com>
2022-01-05 11:20:28 +0530

    Queue of Objects

---
+++ QueueObjDEC14/QueueObj/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueObj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            int num = 3;
+
+            for(int i = 0; i < num; i++)
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                queue.Enqueue(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            for(int i = 0; i < num; i++)
+            {
+                Person p = (Person) queue.Dequeue();
+                Console.WriteLine(p.ToString());
+            }
+        }
+    }
+}
