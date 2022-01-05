6be818af81b4dd36ae86bc39c46289edf9c3056a
mayuriupadhye <upadhyemayu27@gmail.com>
2022-01-05 12:19:05 +0530

    Second Assignment

---
+++ Assignment_2-1/Assignment_2-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std1 = new Student(1, "Mayuri", "C#");
+            Student std2 = new Student(2, "Pooja", "ASP.NET");
+
+            std1.Payment(1000);
+            std2.Payment(500);
+
+            std1.Print();
+            Console.WriteLine();
+            std2.Print();
+            //Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment_2-1/Assignment_2-1/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_1
+{
+    class Student
+    {
+        public int RollNo;
+        public string Name;
+        public string CourseJoined;
+        public int FeePaid;
+
+        public Student(int RollNo,string Name,string CourseJoined)
+        {
+            this.RollNo = RollNo;
+            this.Name = Name;
+            this.CourseJoined = CourseJoined;
+        }
+        public void Payment(int amount)
+        {
+            FeePaid += amount;
+        }
+        public int TotalFee
+        {
+            get
+            {
+                if (CourseJoined.ToLower() == "c#")
+                    return 2000;
+                else if (CourseJoined.ToLower() == "asp.net")
+                    return 3000;
+                return 0;
+            }
+        }
+        public int DueAmount
+        {
+            get
+            {
+                return TotalFee - FeePaid;
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine("Roll No : {0}", RollNo);
+            Console.WriteLine("Name : {0}", Name);
+            Console.WriteLine("Course Joined : {0}", CourseJoined);
+            Console.WriteLine($"Total Fee : {TotalFee}"); ;
+            Console.WriteLine("Fee Paid : {0}", FeePaid);
+            Console.WriteLine("Due Amount : {0}", DueAmount);
+        }
+    }
+}
