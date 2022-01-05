810b94e741b300fe900ddec5073debd1491d2bc0
mayuriupadhye <upadhyemayu27@gmail.com>
2022-01-05 11:18:23 +0530

    ArrayList of Objects

---
+++ ArrayListObjDEC14/ArraylistObj/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace ArraylistObj
+{
+    class Person
+    {
+        public string Name;
+  
+        public int Age;
+  
+
+        public override string ToString()
+        {
+            return Name + " - " + Age;
+        }
+      
+    }
+}
---
+++ ArrayListObjDEC14/ArraylistObj/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace ArraylistObj
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+
+            int total = 3;
+            for (int i = 0; i < total; i++)
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                list.Add(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            foreach (Person p in list)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
