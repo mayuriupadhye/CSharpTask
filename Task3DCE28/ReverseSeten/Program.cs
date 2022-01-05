defb63f767a79e1c146b9feef5f1bd3d0cc783d8
mayuriupadhye <upadhyemayu27@gmail.com>
2022-01-05 11:00:20 +0530

    Reverse the order of words in string

---
+++ Task3DCE28/ReverseSeten/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseSeten
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int temp;
+            Console.WriteLine("Enter the string : ");
+            string s = Console.ReadLine();
+            string[] a = s.Split(' ');
+            int k = a.Length - 1;
+            temp = k;
+            Console.WriteLine("\nReverse string : ");
+            for (int i = k; temp >= 0; k--)
+            {
+                Console.Write(a[temp] + "" + ' ');
+                --temp;
+            }
+            Console.ReadKey();
+        }
+    }
+}
