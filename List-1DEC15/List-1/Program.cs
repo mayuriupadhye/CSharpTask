7d96bce7eb31b53803a64a445d8f461741e26cb4
mayuriupadhye <upadhyemayu27@gmail.com>
2022-01-05 11:11:49 +0530

    Each integer value is added to 2 & the result multiplied by 5

---
+++ List-1DEC15/List-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace List_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> mylist = temp(new List<int>(new int[] { 1, 2, 3, 4 }));
+            foreach (var i in mylist)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+            Console.ReadKey();
+        }
+        public static List<int> temp(List<int> nums)
+        {
+            IEnumerable<int> e = nums.Select(x => 5 * (x + 2));
+            return e.ToList();
+        }
+    }
+}
