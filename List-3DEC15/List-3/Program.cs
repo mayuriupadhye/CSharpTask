d9d3ec680b73dc849cd80bd3f8418ab79b83e959
mayuriupadhye <upadhyemayu27@gmail.com>
2022-01-05 11:15:13 +0530

    Remove all 'a' from string

---
+++ List-3DEC15/List-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_3
+{
+    class Program : RemoveStr
+    {
+        public static void Main()
+        {
+            string s = "java";
+            removeChar(s, 'a');
+            Console.ReadKey();
+        }
+    }
+}
---
+++ List-3DEC15/List-3/RemoveStr.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_3
+{
+    class RemoveStr
+    {
+        public static void removeChar(string s, char c)
+        {
+            int j, count = 0, n = s.Length;
+            char[] t = s.ToCharArray();
+            for (int i = j = 0; i < n; i++)
+            {
+                if (s[i] != c)
+                {
+                    t[j++] = s[i];
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            while (count > 0)
+            {
+                t[j++] = '\0';
+                count--;
+            }
+            Console.Write(t);
+        }
+    }
+}
