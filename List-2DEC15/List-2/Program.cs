a53ddff143f4ec895d94810d0b690f494972386b
mayuriupadhye <upadhyemayu27@gmail.com>
2022-01-05 11:14:01 +0530

    Each element is replaced by 4 copies of string concatenate together

---
+++ List-2DEC15/List-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> mylist = test(new List<string>(new string[] { "1", "2", "3", "4" }));
+            foreach (var i in mylist)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+            Console.ReadKey();
+        }
+        public static List<string> test(List<string> str)
+        {
+            IEnumerable<string> t = str.Select(x => x = x + x + x + x);
+            return t.ToList();
+        }
+    }
+}
