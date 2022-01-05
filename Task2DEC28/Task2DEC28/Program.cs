3c01569b58a693a5c84f6c9c6da854cb1633d20e
mayuriupadhye <upadhyemayu27@gmail.com>
2022-01-05 10:49:03 +0530

    Reverse a string

---
+++ Task2DEC28/Task2DEC28/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2DEC28
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string str1;
+
+            Console.WriteLine("Enter the string :");
+            str1 = Console.ReadLine();
+
+            char[] arr = str1.ToCharArray();
+            Array.Reverse(arr);
+
+            Console.WriteLine(new String(arr));
+            Console.ReadKey();
+        }
+    }
+}
